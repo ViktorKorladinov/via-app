23f8e6931d197bc2343a8b1924bae7755f2b12fa
ViktorKorladinov <v.korladinov@gmail.com>
2022-09-27 16:39:15 +0200

    Added EuropeMap slice.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import europeMapReducer from '../features/europe_map/europeMapSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    europeMap : europeMapReducer,
   },
 });
---
+++ src/features/europe_map/europeMapSlice.js
@@ -0,0 +1,29 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    selectedRegion: "Europe",
+};
+
+export const europeMapSlice = createSlice({
+    name: 'europeMap',
+    initialState,
+    // The `reducers` field lets us define reducers and generate associated actions
+    reducers: {
+        reset: (state) => {
+            state.selectedRegion = initialState.selectedRegion;
+        },
+        // Use the PayloadAction type to declare the contents of `action.payload`
+        changeRegion: (state, action) => {
+            state.selectedRegion = action.payload;
+        },
+    },
+});
+
+export const {reset, changeRegion} = europeMapSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+export const selectRegion = (state) => state.europeMap.selectedRegion;
+
+export default europeMapSlice.reducer;
